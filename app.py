abd1d2248e5bff334cd69bd4d8b9910d86ef937b
kalpana <bkalpana.psg@gmail.com>
2023-06-27 23:01:23 +0530

    uploaded

---
+++ app.py
@@ -0,0 +1,17 @@
+import os
+from constants import openai_key
+os.environ['OPENAI_API_KEY']=openai_key
+
+#https://www.youtube.com/watch?v=u8vQyTzNGVY
+
+import streamlit as st
+from langchain.llms import OpenAI
+
+st.title('🦜️🔗 LangChain')
+prompt=st.text_input('enter your prompt')
+
+llms=OpenAI(temperature=0.9)
+
+if prompt:
+    response=llms(prompt)
+    st.write(response)
